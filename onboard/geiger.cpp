ae67b4be0d6c3643be80c93d8dedf6385f910e65
Venice <25238465+NGC0001@users.noreply.github.com>
2024-04-14 14:14:20 +0100

    Create geiger data file

--- onboard/geiger.cpp
+++ onboard/geiger.cpp
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <ctime>
 #include <iostream>
 #include <filesystem>
 #include <fstream>
@@ -32,9 +33,19 @@ constexpr const char *const PinDirection = "in";
 constexpr const char *const PinEdge = "falling";
 
 constexpr int GeigerPin = 15;
+constexpr const char *const OutputPrefix = "data/geiger_out";
 
 
 void open_gpio_input(int pin) {
+  // unexport pin
+  std::string fGpioUnexport = std::string(GpioDir) + "unexport";
+  EXIT_IF(!std::filesystem::exists(fGpioUnexport), "gpio unexport not exists");
+  std::ofstream ofsGpioUnexport(fGpioUnexport, std::ios_base::binary | std::ios_base::out);
+  EXIT_IF(!ofsGpioUnexport, "failed to open gpio unexport");
+  ofsGpioUnexport << pin << std::flush;
+  if(!ofsGpioUnexport) { std::cerr << "failed to write gpio unexport" << std::endl; }
+  ofsGpioUnexport.close();
+
   // export pin
   std::string fGpioExport = std::string(GpioDir) + "export";
   EXIT_IF(!std::filesystem::exists(fGpioExport), "gpio export not exists");
@@ -122,14 +133,44 @@ void poll_gpio_input(int pin, char *buf, size_t sz, std::function<void(char *, s
 int main(int argc, char *argv[]) {
   (void)argc;
   (void)argv;
+
+  const auto start = std::chrono::system_clock::now();
+  const std::time_t start_t = std::chrono::system_clock::to_time_t(start);
+
+  char startTimeStr[std::size("dd-mm-yy_HH-MM-SS")];
+  int nbytes = std::strftime(std::data(startTimeStr), std::size(startTimeStr),
+      "%d-%m-%y_%H-%M-%S", std::gmtime(&start_t));
+  EXIT_IF(nbytes <= 0, "failed to format time");
+  std::string fOutput = std::string(OutputPrefix) + "_UTC_" + startTimeStr;
+  std::ofstream ofsOutput(fOutput,
+      std::ios_base::binary | std::ios_base::out | std::ios_base::app);
+  EXIT_IF(!ofsOutput, "failed to open output");
+  ofsOutput << "----------------\n" << std::ctime(&start_t) << std::endl;
+  EXIT_IF(!ofsOutput, "failed to write output");
+  auto flushTime = std::chrono::system_clock::now();
+
   char buffer[9];
-  poll_gpio_input(GeigerPin, buffer, sizeof buffer - 1, [](char *buf, size_t cnt) {
-      buf[cnt] = 0;
-      const auto now = std::chrono::system_clock::now();
-      const std::time_t now_t = std::chrono::system_clock::to_time_t(now);
-      const auto c = std::chrono::duration_cast<std::chrono::nanoseconds>(
-          std::chrono::system_clock::now() - now).count();
-      std::cout << std::ctime(&now_t) << " : " << buf << " : " << c << "\n";
+  poll_gpio_input(GeigerPin, buffer, sizeof buffer - 1,
+      [start, &flushTime, &ofsOutput] (char *buf, size_t cnt) {
+        buf[cnt] = 0;
+        // The time functions consume around 10-20us.
+        const auto now = std::chrono::system_clock::now();
+        const auto lapse = now - start;
+        ofsOutput << std::chrono::duration_cast<std::chrono::nanoseconds>(lapse).count()
+                  << " " << buf;
+        EXIT_IF(!ofsOutput, "failed to write output");
+        const auto flushLapse = now - flushTime;
+        if (std::chrono::duration_cast<std::chrono::milliseconds>(flushLapse).count() >= 100) {
+          // flush every 100ms
+          ofsOutput << std::flush;
+          flushTime = now;
+        }
+        // TODO: remove following
+        std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(
+                       std::chrono::system_clock::now() - now).count()
+                  << "ns\n";  // around 200-300ns
   });
+
+  ofsOutput.close();
   return 0;
 }
